bd3f10b2532dc58f2eb85615af967a9ad6df5d47
Ben Windsor <benwindsor@gmail.com>
2017-06-02 10:34:31 +0200

    Fixed bug deleting last thing in list

--- app/src/main/java/com/github/bwindsor/pairlearnapp/EditCategoryActivity.java
+++ app/src/main/java/com/github/bwindsor/pairlearnapp/EditCategoryActivity.java
@@ -123,7 +123,7 @@ public class EditCategoryActivity extends AppCompatActivity implements AdapterVi
 
             private void deleteSelectedItems() {
                 List<Integer> pairIdsToRemove = new ArrayList<>();
-                for (int i = 0; i < mIsSelected.size()-1; i++) {
+                for (int i = 0; i < mIsSelected.size(); i++) {
                     if (mIsSelected.get(i)) {
                         mCursor.moveToPosition(i);
                         pairIdsToRemove.add(mCursor.getInt(mCursor.getColumnIndex(WordsContract.Pairs.WORD_PAIR_ID)));
